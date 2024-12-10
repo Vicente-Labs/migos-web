62fe30dd903d9c9a767df34b54618f218c3f31a0
Vicentesan <vikom.sanchez@gmail.com>
2024-12-10 12:49:00 -0300

    chore

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -138,7 +138,7 @@ export default function Home() {
 
       <div className="w-full max-w-7xl px-4">
         <div className="mb-8 rounded-lg bg-yellow-100 p-4 text-center font-poppins text-yellow-800">
-          ⚠️ {dictionary.launchDelayed}
+          ⚠️ {dictionary.importantNotice}: {dictionary.launchDelayedOnly}
         </div>
       </div>
 
